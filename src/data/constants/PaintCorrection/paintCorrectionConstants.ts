976ff48c9b4666a7fefa6473eaae5329aa661f07
Sh0bhit <sguptamds94@gmail.com>
2023-11-17 16:21:19 +0530

    Pricing Change

--- src/data/constants/PaintCorrection/paintCorrectionConstants.ts
+++ src/data/constants/PaintCorrection/paintCorrectionConstants.ts
@@ -12,16 +12,16 @@ export const paintCorrectionPackagedata: paintCorrectionPackagedataType[] = [
   {
     heading: "Car Value Booster",
     desc: "Sleek Custom’s special car value booster pack includes a full iron decontamination, a 1 stage paint correction, a nice shiny car show wax, interior detailed and your windows ceramic coated which makes your car as sleek as new!",
-    pricing: 397,
+    pricing: 597,
   },
   {
     heading: "1 STEP POLISH",
     desc: "Stage 1 paint correction is recommended for vehicles with minimal paint defects for example hard water spots and light swirls. Correction in this stage is aimed to improve the overall gloss and shine of your paints finish. This package is recommended for newer vehicles",
-    pricing: 397,
+    pricing: 597,
   },
   {
     heading: "FULL CORRECTION",
     desc: "Sleek Custom’s stage 2 was one of our most popular paint correction services. This service includes a multi stage correction that can remove up to 80% of swirls, scratches and other imperfections on automotive paint. This service will restore the color and clarity to any vehicles finish. Pristine detail stage 2 correction can make your vehicle looking new again.",
-    pricing: 497,
+    pricing: 797,
   },
 ];
